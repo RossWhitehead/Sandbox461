dd649386080fff0d04e729befd90193684499576
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2015-12-17 18:13:49 +0000

    Initial create

---
+++ Sandbox461/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sandbox461.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            ViewBag.Message = "Upload page.";
+
+            return View();
+        }
+
+        public ActionResult UploadAjax(List<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                //var filename = Path.Combine(Server.MapPath("~/App_Data"), file.FileName);
+                //file.SaveAs(filename);
+            }
+
+            return Json(files.Select(x => new { name = x.FileName }));
+        }
+    }
+}
---
+++ Sandbox461/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sandbox461.Startup))]
+namespace Sandbox461
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
